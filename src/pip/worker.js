98ed98fb8efc94c58586e6dbce89b0cadb92a20c
Jeremy Castagno <JeremyBYU@gmail.com>
2023-10-06 12:04:26 -0400

    feat(startup): Improve startup time

--- src/pip/worker.js
+++ src/pip/worker.js
@@ -8,6 +8,8 @@
 
 const logger = require( 'pelias-logger').get('admin-lookup:worker');
 const PolygonLookup = require('polygon-lookup');
+const v8 = require('v8'); 
+const RBush = require('rbush');
 
 const readStream = require('./readStream');
 const fs = require('fs');
@@ -31,19 +33,25 @@ process.on('SIGTERM', () => {
   process.exit(0);
 });
 
-readStream(datapath, layer, localizedAdminNames, (features) => {
-  // find all the properties of all features and write them to a file
-  // at the same time, limit the feature.properties to just Id and Hierarchy since it's all that's needed in the worker
-  const data = features.reduce((acc, feature) => {
-    acc[feature.properties.Id] = feature.properties;
-    feature.properties = {
-      Id: feature.properties.Id,
-      Hierarchy: feature.properties.Hierarchy
-    };
-    return acc;
-  }, {});
+const serializedAdminLookupPath = path.join(datapath, 'serialized', `${layer}.v8`);
 
-  adminLookup = new PolygonLookup( { features: features } );
+if (fs.existsSync(serializedAdminLookupPath)) {
+  logger.info(`Found serialized data at ${serializedAdminLookupPath}. Attempting to deserialize...`);
+  const adminDataSer = fs.readFileSync(serializedAdminLookupPath);
+  const adminData = v8.deserialize(adminDataSer);
+
+  // let adminLookupSer = fs.readFileSync(serializedAdminLookupPath);
+  const adminLookupTemp = adminData.adminLookup;
+  const data = adminData.data;
+
+  // Create the adminLookup using our deserialized data
+  adminLookup = new PolygonLookup( { features: [] } );
+  const rtree = new RBush();
+  rtree._maxEntries = adminLookupTemp.rtree._maxEntries;
+  rtree._minEntries = adminLookupTemp.rtree._minEntries;
+  rtree.data = adminLookupTemp.rtree.data;
+  adminLookup.rtree = rtree;
+  adminLookup.polygons = adminLookupTemp.polygons;
 
   process.on('message', msg => {
     switch (msg.type) {
@@ -59,7 +67,64 @@ readStream(datapath, layer, localizedAdminNames, (features) => {
     data: data,
     seconds: ((Date.now() - startTime)/1000)
   });
-});
+
+}
+else
+{
+  readStream(datapath, layer, localizedAdminNames, (features) => {
+    // find all the properties of all features and write them to a file
+    // at the same time, limit the feature.properties to just Id and Hierarchy since it's all that's needed in the worker
+    const data = features.reduce((acc, feature) => {
+      acc[feature.properties.Id] = feature.properties;
+      feature.properties = {
+        Id: feature.properties.Id,
+        Hierarchy: feature.properties.Hierarchy
+      };
+      return acc;
+    }, {});
+    adminLookup = new PolygonLookup( { features: features } );
+    // Add serialization of data
+    let doSerialization = true;
+    if (!fs.existsSync(path.dirname(serializedAdminLookupPath))){
+      try {
+        fs.mkdirSync(path.dirname(serializedAdminLookupPath));
+      } catch (error) {
+        if (error.code === 'EEXIST')
+        {
+          // multiprocessor application. existsSync may be stale.
+          logger.warn(`${path.dirname(serializedAdminLookupPath)} already exists, skip creation`);
+        }
+        else{
+          logger.error(`Error creating ${path.dirname(serializedAdminLookupPath)}, skipping serialization... `);
+          doSerialization = false;
+        }
+      }
+    }
+    if (doSerialization)
+    {
+      logger.info(`Serializing data into ${serializedAdminLookupPath}`);
+      const adminData = { data: data, adminLookup: adminLookup};
+      let adminDataSer = v8.serialize(adminData);
+      fs.writeFileSync(serializedAdminLookupPath, adminDataSer);
+    }
+  
+    process.on('message', msg => {
+      switch (msg.type) {
+        case 'search' : return handleSearch(msg);
+        default       : logger.error('Unknown message:', msg);
+      }
+    });
+  
+    // alert the master thread that this worker has been loaded and is ready for requests
+    process.send( {
+      type: 'loaded',
+      layer: layer,
+      data: data,
+      seconds: ((Date.now() - startTime)/1000)
+    });
+  });
+
+}
 
 function handleSearch(msg) {
   process.send({
